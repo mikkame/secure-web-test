1d23d59176151ca2a33230a1619790bb5fb65aef
mikami <mikami@tecpresso.co.jp>
2016-08-30 11:22:13 +0900

    add css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO
     exit;
 }
 setcookie('test', 'test', 0, '/', $_SERVER["HTTP_HOST"], true, true);
-header("Content-Security-Policy: default-src 'self'");
+header("Content-Security-Policy: default-src 'self' *.jquery.com");
 header('Strict-Transport-Security: max-age=31536000');
 header("X-Content-Type-Options: nosniff");
 header("X-Frame-Options: DENY");
@@ -17,9 +17,13 @@ header("X-XSS-Protection: 1; mode=block");
 	<head>
 		<meta charset="utf-8" />
 		<title>Secure Web Test</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
 		<script src="https://code.jquery.com/jquery-2.1.4.min.js" integrity="sha384-R4/ztc4ZlRqWjqIuvf6RX5yb/v90qNGx6fS48N0tRxiGkqveZETq72KgDVJCp2TC" crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<h1>Hello World</h1>
+		<h1>Secure Web Test</h1>
+		<span id="forkongithub">
+			<a href="https://github.com/codepo8/css-fork-on-github-ribbon">Fork me on GitHub</a>
+		</span>
 	</body>
 </html>
